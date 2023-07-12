32fcf46916c1cc67dd84344e65e3fe011c2aa988
ajtwede <ajtwede@gmail.com>
2023-07-12 09:32:52 -0600

    day 1

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+openai.api_key_path = 'openai_api_key.txt'
+openai.organization = "org-RIvFKMaDPV0lPROuwzxHppHM"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        song = request.form["animal"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(song),
             temperature=0.6,
+            max_tokens=256
+
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +26,7 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(song):
+    result = "Suggest 5 songs that are similar to {}. and format response as an html list".format(song.capitalize())
+    print(result)
+    return result
