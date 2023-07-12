2f19144f515dcac1a70cde0b56022b89a72a1b96
andywakefield <439568+andywakefield@users.noreply.github.com>
2023-07-12 15:18:38 -0600

    added spotify integration basics

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ import os
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
+# import spotipy
+# from spotipy.oauth2 import SpotifyClientCredentials
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    sp_test()
     if request.method == "POST":
         song = request.form["animal"]
         response = openai.Completion.create(
@@ -31,3 +38,29 @@ def generate_prompt(song):
     print(result)
     return result
 
+def sp_test() :
+    # auth_manager = SpotifyClientCredentials()
+    # sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    # playlists = sp.user_playlists('spotify')
+
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    playlists = sp.user_playlists('spotify')
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+    # urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
+    # auth_manager = SpotifyClientCredentials()
+    # sp = spotipy.Spotify(auth_manager=auth_manager)
+    
+    # artist = sp.artist(urn)
+    # print(artist)
+
+    # user = sp.user('plamere')
+    # print(user)
