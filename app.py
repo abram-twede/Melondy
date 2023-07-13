0d58821938ff3252238a9fb8a79e3f650d40e6a1
ajtwede <ajtwede@gmail.com>
2023-07-13 08:36:31 -0600

    fix

--- app.py
+++ app.py
@@ -1,80 +1,80 @@
-import os
-import openai
-from flask import Flask,request, redirect, session, render_template, request, url_for
-
-# import spotipy
-# from spotipy.oauth2 import SpotifyClientCredentials
-
+from flask import Flask, request, redirect, session, render_template, url_for
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy import Spotify
+import openai
+import os
 from spotipy.oauth2 import SpotifyOAuth
 
-
 app = Flask(__name__)
+app.secret_key = os.getenv('APP_CLIENT_SECRET')
 openai.api_key = os.getenv("OPENAI_API_KEY")
-sp_oauth = SpotifyOAuth(
-    client_id='your-spotify-client-id',
-    client_secret='your-spotify-client-secret',
-    redirect_uri=url_for('callback', _external=True),
-    scope='playlist-modify-public user-read-email'
-)
 
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        scope='playlist-modify-public user-read-email'
+    )
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    sp_test()
-    if request.method == "POST":
-        song = request.form["animal"]
+@app.route('/')
+def login():
+    if 'token_info' in session:
+        return redirect(url_for('create_playlist')) 
+    else:
+        sp_oauth = create_spotify_oauth()
+        code = request.args.get('code')
+        if code:
+            token_info = sp_oauth.get_access_token(code=code)
+            session["token_info"] = token_info
+            return redirect(url_for('create_playlist'))
+        else:
+            auth_url = sp_oauth.get_authorize_url()
+            return render_template('login.html', auth_url=auth_url)  
+
+@app.route('/callback')
+def callback():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code=code)
+    session["token_info"] = token_info
+    return redirect(url_for('create_playlist'))
+
+@app.route('/create_playlist', methods=['GET', 'POST'])
+def create_playlist():
+    if 'token_info' not in session:
+        return redirect(url_for('login'))  
+
+    if request.method == 'POST':
+        song = request.form.get('song')
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(song),
             temperature=0.6,
             max_tokens=256
-
         )
         result = response.choices[0].text
         print(result)
-        return redirect(url_for("index", result=result))
-
+        return redirect(url_for("create_playlist", result=result))
     result = request.args.get("result")
-    return render_template('index.html', result=result)
+    return render_template('index.html', result=result)  
 
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    sp_oauth = create_spotify_oauth()
+    if token_info and not sp_oauth.is_token_expired(token_info):
+        token_valid = True
+    else:
+        code = request.args.get('code')
+        if code:
+            token_info = sp_oauth.get_access_token(code=code)
+            session["token_info"] = token_info
+            token_valid = True
+
+    return token_info, token_valid
 
 def generate_prompt(song):
-    result = "Suggest 5 songs that are similar to {}. and add a newline after each item".format(song.capitalize())
-    print(result)
-    return result
+    return "Suggest 5 songs that are similar to {}. and add a newline after each item".format(song.capitalize())
 
-def sp_test() :
-    # auth_manager = SpotifyClientCredentials()
-    # sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    # playlists = sp.user_playlists('spotify')
-
-    auth_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    playlists = sp.user_playlists('spotify')
-    while playlists:
-        for i, playlist in enumerate(playlists['items']):
-            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-        if playlists['next']:
-            playlists = sp.next(playlists)
-        else:
-            playlists = None
-    # urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
-    # auth_manager = SpotifyClientCredentials()
-    # sp = spotipy.Spotify(auth_manager=auth_manager)
-    
-    # artist = sp.artist(urn)
-    # print(artist)
-
-    # user = sp.user('plamere')
-    # print(user)
-
-@app.route('/login')
-def login():
-    auth_url = sp_oauth.get_authorize_url()
-    # Render the login.html template with auth_url as a parameter
-    return render_template('login.html', auth_url=auth_url)
+if __name__ == "__main__":
+    with app.app_context():  # Creating application context
+        app.run(debug=True)
